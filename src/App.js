29393fd3e381aa335c3ad19c86f9da9dfb7ffa2d
Pusaka Manggala <pusakamanggala@gmail.com>
2023-03-07 20:11:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import WeatherInfo from "./components/WeatherInfo";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App bg-mainColor h-screen flex justify-center text-white items-center">
+        <WeatherInfo />
+      </div>
+    </QueryClientProvider>
   );
 }
 
